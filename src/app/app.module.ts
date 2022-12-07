3dde1d4db91e05640ddefd03d03e259b328d4768
TimKardol <tnmkardol@outlook.com>
2022-12-07 15:13:50 +0100

    Add dragdropmodules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 import { AppRoutingModule } from './app-routing.module';
 
 // NG Translate
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
 
 import { AppComponent } from './app.component';
 import { MainWindowComponent } from './main-window/main-window.component';
@@ -24,12 +26,16 @@ const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>  new Transl
 @NgModule({
   declarations: [AppComponent, MainWindowComponent, PackageComponent, ConnectorComponent, BuildsComponent],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    DragDropModule,
     FormsModule,
     HttpClientModule,
     CoreModule,
     SharedModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
